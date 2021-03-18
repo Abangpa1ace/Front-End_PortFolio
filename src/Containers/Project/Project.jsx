cb59728b0cbfef32087ec31536acd04c74022362
Abangpa1ace <dyek72731@gmail.com>
2021-03-18 17:52:28 +0900

    Project 작성중

--- src/Containers/Project/Project.jsx
+++ src/Containers/Project/Project.jsx
@@ -7,6 +7,35 @@ const Project = () => {
     <section id="Project">
       <div className="about-container">
         <Title>My ProJect</Title>
+        <ul className="project-container">
+          <li className="project-item">
+            <div className="project-img">
+              <img src="./Images/background.jpeg" alt="project-img" />
+            </div>
+            <div className="project-desc">
+              <h3><span>[Group]</span> 배민문방구 클론 프로젝트</h3>
+            </div>
+          </li>
+
+          <li className="project-item">
+            <div className="project-img">
+              <img src="./Images/background.jpeg" alt="project-img" />
+            </div>
+            <div className="project-desc">
+            <h3><span>[Personal]</span> 에어비앤비 클론 프로젝트</h3>
+            </div>
+          </li>
+
+          <li className="project-item">
+            <div className="project-img">
+              <img src="./Images/background.jpeg" alt="project-img" />
+            </div>
+            <div className="project-desc">
+            <h3><span>[Personal]</span> 나이키 클론 프로젝트</h3>
+            </div>
+          </li>
+
+        </ul>
       </div>
     </section>
   )
